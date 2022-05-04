999e1000634f5fffab08325dd6e7bc07ddc6591f
Toygun Demirkaya <vtoygund@gmail.com>
2022-05-04 17:09:55 +0200

    logo and navbar updated

--- src/components/NavbarCom/index.js
+++ src/components/NavbarCom/index.js
@@ -1,29 +1,44 @@
-import { Link } from "react-router-dom";
+import {Link,Route} from "react-router-dom";
 import styles from "./Navbar.module.css";
 import { AuthContext } from "context";
 import { useContext } from "react";
-import {Nav, Navbar, Container} from "react-bootstrap"
+import {Nav, Navbar, Container, img} from "react-bootstrap"
 
 
 export function NavbarCom() {
   const { logout, user} = useContext(AuthContext);
   return (
-    <Navbar bg="light" expand="lg">
-    <Container>
-      <Navbar.Brand href="/">TEATALK </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-      {user && <Nav.Link href="/AddPost"> Add Post </Nav.Link>}
-      {!user && <Nav.Link href="/login"> Login </Nav.Link>}
-      {!user && <Nav.Link href="/signup"> Sign Up </Nav.Link>}
-      {user && <Nav.Link href="/"> <p>{user.userName}</p></Nav.Link>}
-      </Nav>
-      <Navbar.Text>
-      {user && <Nav.Link onClick={logout}> Logout </Nav.Link>}
-      </Navbar.Text>
-      </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+  <div>
+    <div className="row">
+      <div className="col-md-12">
+        <Navbar bg="light" expand="lg" sticky="top">
+            <Navbar.Brand href="/">
+              <img
+                alt="Logo"
+                src="/teatalk-logo.png"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+             />
+            TEATALK 
+          </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                {user && <Nav.Link href="/AddPost"> Add Post </Nav.Link>}
+                {!user && <Nav.Link href="/login"> Login </Nav.Link>}
+                {!user && <Nav.Link href="/signup"> Sign Up </Nav.Link>}
+                {/* {user && <Nav.Link href="/"> <p>{user.userName}</p></Nav.Link>} */}
+              </Nav>
+                <Navbar.Text>
+                {user && <Nav.Link onClick={logout}> Logout </Nav.Link>}
+              </Navbar.Text>
+            </Navbar.Collapse> 
+        </Navbar>                 
+      </div>
+    </div>
+  </div>
+  </>
   );
 }
