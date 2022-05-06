60823114520ad37614af2eabf8a77d9f5afd55fd
Toygun Demirkaya <vtoygund@gmail.com>
2022-05-06 14:33:08 +0200

    signup title fix

--- src/pages/SignupPage/index.js
+++ src/pages/SignupPage/index.js
@@ -1,14 +1,19 @@
 import { NavbarCom, Signup } from "components";
 import { AuthContext } from "context";
 import { useContext } from "react";
+import styles from "./Signup.module.css";
 
 export function SignupPage() {
   const { user } = useContext(AuthContext);
   return (
     <div>
       <NavbarCom />
-      <h3 className="mx-4 mt-4">Let`s Start TeaTalk</h3>
+      <div className={styles.listContainer}>
+       <div style={{ width: "50rem" }}>
+        <h3>Let`s Start TeaTalk</h3>
+       </div>
       <Signup />
+      </div>
     </div>
   );
 }
