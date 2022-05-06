a216c9ea7daa29f1b51d6784ad1c515ed3fbf071
Toygun Demirkaya <vtoygund@gmail.com>
2022-05-06 14:42:17 +0200

    chanfed signup login

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,19 +1,14 @@
 import { NavbarCom, Login } from "components";
 import { AuthContext } from "context";
 import { useContext } from "react";
-import styles from "./Login.module.css";
 
 export function LoginPage() {
   const { user } = useContext(AuthContext);
   return (
     <div>
       <NavbarCom />
-      <div className={styles.listContainer}>
-       <div style={{ width: "50rem" }}>
-        <h3>Login before your tea get cold</h3>
-      </div>
+      <h3 className="mx-2">Login before your tea get cold</h3>
       <Login />
-      </div>
     </div>
   );
 }
--- src/pages/SignupPage/index.js
+++ src/pages/SignupPage/index.js
@@ -1,19 +1,14 @@
 import { NavbarCom, Signup } from "components";
 import { AuthContext } from "context";
 import { useContext } from "react";
-import styles from "./Signup.module.css";
 
 export function SignupPage() {
   const { user } = useContext(AuthContext);
   return (
     <div>
       <NavbarCom />
-      <div className={styles.listContainer}>
-       <div style={{ width: "50rem" }}>
-        <h3>Let`s Start TeaTalk</h3>
-       </div>
+      <h3 className="mx-2">Let`s Start TeaTalk</h3>
       <Signup />
-      </div>
     </div>
   );
 }
